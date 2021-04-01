Tried to fix rotation

tetromino.py
@@ -96,10 +96,14 @@ class Tetromino:
                if position.y < self.grid_height:
                   self.tile_matrix[row][col].draw() 
    
-   # IMPLEMENT CAN ROTATABLE
+   """
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   CAN ROTATETABLE IMPLEMENTATION REQUIRED
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   """
    def rotate(self):
       copy_matrix = np.rot90(self.tile_matrix.copy())
-      self.tile_matrix = np.full((len(copy_matrix), len(copy_matrix)), None)
+      new_tile_matrix = np.full((len(copy_matrix), len(copy_matrix)), None)
 
       for i in range(len(copy_matrix)):
          for j in range(len(copy_matrix)):
@@ -107,7 +111,9 @@ class Tetromino:
                position = Point()
                position.x = self.bottom_left_corner.x + j
                position.y = self.bottom_left_corner.y + (len(copy_matrix) - 1) - i
-               self.tile_matrix[i][j] = Tile(position)
+               new_tile_matrix[i][j] = Tile(position)
+
+      self.tile_matrix = new_tile_matrix
 
    # Method for moving the tetromino in a given direction by 1 on the game grid
    def move(self, direction, game_grid):